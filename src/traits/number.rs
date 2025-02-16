b145d10f3cd5083cae6fc171452ff33d25ee09be
Prokop <prokop.schield@protonmail.com>
2025-02-16 16:54:05 +0100

    feat(src/traits/number.rs): impl<T> Number for &T

--- src/traits/number.rs
+++ src/traits/number.rs
@@ -94,3 +94,65 @@ macro_rules! impl_number_for {
 }
 
 impl_number_for!(u8, u16, u32, u64, u128, i8, i16, i32, i64, i128, f32, f64, usize, isize);
+
+impl<T: Number> Number for &T {
+    /// Returns the real part of this number.
+    #[inline]
+    fn real(&self) -> f64 {
+        (**self).real()
+    }
+
+    /// Returns the imaginary part of this number.
+    #[inline]
+    fn imag(&self) -> f64 {
+        (**self).imag()
+    }
+
+    /// Computes the magnitude (absolute value) of this number.
+    #[inline]
+    fn magnitude(&self) -> f64 {
+        (**self).magnitude()
+    }
+
+    /// Computes the phase (angle) of the number in radians.
+    #[inline]
+    fn phase(&self) -> f64 {
+        (**self).phase()
+    }
+
+    /// Computes the sum of two numbers.
+    #[inline]
+    fn add(&self, rhs: impl Number) -> Cartesian<f64, f64> {
+        (**self).add(rhs)
+    }
+
+    /// Computes the difference of two numbers.
+    #[inline]
+    fn sub(&self, rhs: impl Number) -> Cartesian<f64, f64> {
+        (**self).sub(rhs)
+    }
+
+    /// Computes the product of two numbers in cartesian form.
+    #[inline]
+    fn cartesian_mul(&self, rhs: impl Number) -> Cartesian<f64, f64> {
+        (**self).cartesian_mul(rhs)
+    }
+
+    /// Computes the product of two numbers in polar form.
+    #[inline]
+    fn polar_mul(&self, rhs: impl Number) -> Polar<f64, f64> {
+        (**self).polar_mul(rhs)
+    }
+
+    /// Computes the product of two numbers.
+    #[inline]
+    fn mul(&self, rhs: impl Number) -> Polar<f64, f64> {
+        (**self).mul(rhs)
+    }
+
+    /// Computes the quotient of two numbers.
+    #[inline]
+    fn div(&self, rhs: impl Number) -> Polar<f64, f64> {
+        (**self).div(rhs)
+    }
+}
